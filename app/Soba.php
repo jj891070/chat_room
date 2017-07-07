b58eabf1c22cb47c14815c78bfcde2dc96587c3a
jay_liao <chungyojay0206@gmail.com>
2017-07-07 18:03:02 +0800

    first upload

---
+++ app/Soba.php
@@ -0,0 +1,9 @@
+<?php
+namespace App;
+
+class Soba{
+	public function work()
+	{
+		echo "i'm working";
+	}
+}
---
+++ resources/assets/js/app.js
@@ -0,0 +1,70 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+Vue.component('example', require('./components/Example.vue'));
+Vue.component('chat-message', require('./components/ChatMessage.vue'));
+Vue.component('chat-log', require('./components/ChatLog.vue'));
+Vue.component('chat-composer', require('./components/ChatComposer.vue'));
+
+
+const app = new Vue({
+    el: '#app',
+    data:{
+      messages:[],
+      userInRoom:[]
+    },
+
+    methods:{
+      addMessage(message){
+        //Add to existing message
+        this.messages.push(message);
+        //Persist to the database etc
+        axios.post('/messages', message).then(response => {
+                // Do whatever;
+        })
+      }
+    },
+
+    created(){
+      axios.get('/messages').then(response=>{
+        this.messages = response.data;
+      });
+    
+
+      Echo.join('chatroom')
+          .here((user)=>{
+            this.userInRoom = user;
+          })
+          .joining((user)=>{
+            this.userInRoom.push(user);
+          })
+          .leaving((user)=>{
+            console.log(user);
+            this.userInRoom = this.userInRoom.filter(u1 => u1!=user);
+            console.log(user.name);
+          })
+          .listen('MessagePosted', (e) => {
+            //Handle event
+            this.messages.push({
+              message: e.message.message,
+              user: e.user
+            });
+
+          });
+    }
+
+});
